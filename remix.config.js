983705175411aa5ce207d140bdf6ffd370a9383f
Michael Wu <mw10013@gmail.com>
2023-12-14 20:01:44 -0500

    remix-auth-totp-dev

--- remix.config.js
+++ remix.config.js
@@ -12,4 +12,10 @@ export default {
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // publicPath: "/build/",
+  serverNodeBuiltinsPolyfill: {
+    modules: { buffer: true, crypto: true },
+    globals: {
+      Buffer: true,
+    },
+  },
 };
