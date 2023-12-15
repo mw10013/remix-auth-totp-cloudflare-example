7461d62da83bae6593e11ca91cf83cf48a46eed1
Michael Wu <mw10013@gmail.com>
2023-12-15 03:18:31 -0500

    account

--- app/lib/hooks.server.ts
+++ app/lib/hooks.server.ts
@@ -1,8 +1,11 @@
+import { createCookieSessionStorage } from "@remix-run/cloudflare";
 import { z } from "zod";
+import { Authenticator } from "remix-auth";
+import { TOTPStrategy } from "remix-auth-totp-dev";
 
 export const cloudflareEnvSchema = z.object({
   ENVIRONMENT: z.string().min(1),
-  COOKIE_SECRET: z.string().min(1),
+  SESSION_SECRET: z.string().min(1),
   //   KV: z.record(z.unknown()).transform((obj) => obj as unknown as KVNamespace),
   //   DB: z.record(z.unknown()).transform((obj) => obj as unknown as D1Database),
   // SMN_R2: z.record(z.unknown()).transform((obj) => obj as unknown as R2Bucket),
@@ -10,11 +13,34 @@ export const cloudflareEnvSchema = z.object({
 
 export type CloudflareEnv = z.infer<typeof cloudflareEnvSchema>;
 
-export function hookCloudflareEnv(env: unknown) {
+export function hookEnv(env: unknown) {
   function assertCloudflareEnv(obj: unknown): asserts obj is CloudflareEnv {
     cloudflareEnvSchema.parse(obj);
   }
   assertCloudflareEnv(env);
-  return env;
+  return { env };
 }
 
+interface User {
+  id: string;
+  email: string;
+  //   createdAt: Date | null;
+  //   updatedAt: Date | null;
+}
+
+export function hookAuth(env: CloudflareEnv) {
+  const authSessionStorage = createCookieSessionStorage({
+    cookie: {
+      name: "_auth",
+      path: "/",
+      sameSite: "lax",
+      httpOnly: true,
+      secrets: [env.SESSION_SECRET],
+      secure: env.ENVIRONMENT === "production",
+    },
+  });
+  const authenticator = new Authenticator<User>(authSessionStorage, {
+    throwOnError: true,
+  });
+  return { authenticator };
+}
--- app/routes/account.tsx
+++ app/routes/account.tsx
@@ -5,17 +5,22 @@ import { hookAuth, hookEnv } from "~/lib/hooks.server";
 export async function loader({ request, context }: LoaderFunctionArgs) {
   const { env } = hookEnv(context.env);
   const { authenticator } = hookAuth(env);
-  return {
-    env,
-    authenticator,
-  };
+  const session = await authenticator.isAuthenticated(request, {
+    failureRedirect: "/signin-up",
+  });
+
+  //   const user = await prisma.user.findUnique({ where: { id: session.id } })
+  //   if (!user) return redirect('/login')
+
+  //   return json({ user } as const)
+  return { authenticator, session };
 }
 
 export default function Route() {
   const data = useLoaderData<typeof loader>();
   return (
     <div>
-      Sign In/Up
+      Account
       <pre>{JSON.stringify(data, null, 2)}</pre>
     </div>
   );
