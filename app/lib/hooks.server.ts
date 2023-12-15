6af3bab13c256d6aa7ea4a05b1cadcdaf13bbd2d
Michael Wu <mw10013@gmail.com>
2023-12-15 10:30:07 -0500

    totp strategy

--- app/lib/hooks.server.ts
+++ app/lib/hooks.server.ts
@@ -42,5 +42,35 @@ export function hookAuth(env: CloudflareEnv) {
   const authenticator = new Authenticator<User>(authSessionStorage, {
     throwOnError: true,
   });
+  authenticator.use(
+    new TOTPStrategy(
+      {
+        secret: "SECRET",
+        magicLinkGeneration: { callbackPath: "/magic-link" },
+
+        storeTOTP: async (data) => {
+          console.log("storeTOTP:", data);
+        },
+        sendTOTP: async ({ email, code, magicLink }) => {
+          console.log("sendTOTP:", { email, code, magicLink });
+        },
+        handleTOTP: async (hash, data) => {
+          console.log("handleTOTP:", { hash, data });
+          return {
+            hash,
+            attempts: 0,
+            active: true,
+            expiresAt: new Date(Date.now() + 60 * 1000),
+          };
+        },
+      },
+      async ({ email }) => {
+        return {
+          id: "userid789",
+          email,
+        };
+      },
+    ),
+  );
   return { authenticator };
 }
