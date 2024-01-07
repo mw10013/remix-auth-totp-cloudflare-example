3d165a509f8f36836939a399592a3bf570adeeda
Michael Wu <mw10013@gmail.com>
2024-01-07 16:06:42 -0500

    cleanup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,7 +5,6 @@ import type { Config } from "tailwindcss";
 export default {
   content: [
     "./app/**/*.{js,jsx,ts,tsx}",
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
