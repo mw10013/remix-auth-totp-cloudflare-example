63b1aa9e9d5189d541f7dfe65f4043348b2ba736
Michael Wu <mw10013@gmail.com>
2024-02-03 05:46:06 -0500

    config

--- env.d.ts
+++ env.d.ts
@@ -1,12 +1,17 @@
 /// <reference types="@remix-run/cloudflare" />
 /// <reference types="vite/client" />
 
-import type { KVNamespace } from "@cloudflare/workers-types";
+import type { D1Database, KVNamespace } from "@cloudflare/workers-types";
 
 declare module "@remix-run/cloudflare" {
   interface AppLoadContext {
     env: {
       KV: KVNamespace;
+      D1: D1Database;
+      ENVIRONMENT: string;
+      SESSION_SECRET: string;
+      TOTP_SECRET: string;
+      RESEND_API_KEY: string;
     };
   }
 }
